57381f75e31542cccb8c347eab848df3cd4358be
aname4405 <aname4405@gmail.com>
2024-02-06 16:23:02 +0000

    added to do comments

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+//To Do: make the form work - get user input on button click and save it to session storage.
 let getInput = (something) => {
     return something;
 };
+
+//maybe have another page where we can create a list of subscriptions to get the details from session storage.
 
 export default () => (
     <form>
@@ -10,6 +13,6 @@ export default () => (
         <hr style={{height:'5px', opacity:'1', backgroundColor:'black'}}></hr>
         <label>Name:</label><input name="name" type="text" />
         <br /><br /><label>Email:</label><input name="email" type="email" />
-        <br /><br /><input type='button' onInput={getInput()} value='Subscribe'/>
+        <br /><br /><input type='button' onClick={getInput()} value='Subscribe'/>
     </form>    
 );
