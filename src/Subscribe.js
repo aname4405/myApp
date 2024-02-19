4c71c0ff70696c48e24934fc848c3821824c8784
aname4405 <aname4405@gmail.com>
2024-02-19 17:25:24 +0000

    json-server  working and subscribe  fetch created

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-//To Do: make the form work - get user input on button click and save it to session storage.
-let getInput = (something) => {
-    return something;
+const Subscribe = () => {
+    const [formData, setForm] = useState({});
+
+    function submitForm(e){
+        e.preventDefault();
+
+        fetch("http://localhost:8000/subscribers", {
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+          
+            
+        for(let key in formData) {
+            sessionStorage.setItem(key, formData[key])
+        };
+    };
+
+    return (
+        <div className='container'>
+        <div className='header'>
+            <div className='title'>Subscribe</div>
+            <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+            <div className='input'>
+                {/* <img src={profile} alt='silhouette icon image of a person' /> */}
+                <input type="text" id="firstName" name="firstName" placeholder="First Name" onInput={e=>setForm({...formData, firstname: e.target.value})} />
+            </div>
+            <div className='input'>
+                {/* <img src={profile} alt='silhouette icon image of a person' /> */}
+                <input type="text" id="surname" name="surname" placeholder="Surname" onInput={e=>setForm({...formData, surname: e.target.value})} />
+            </div>
+            <div className='input'>
+                {/* <img src={email} alt='silhouette icon image of an email evelope' /> */}
+                <input type="email" id="email" name="email" placeholder="Email Address" onInput={e=>setForm({...formData, email: e.target.value})} />
+            </div>
+        </div>
+        <div className='submitContainer'>
+            <buttonLink onClick={e=>submitForm(e)} ><Link className="link" to="/">SUBSCRIBE</Link></buttonLink>
+        </div>
+    </div>
+    );
+
 };
 
-//maybe have another page where we can create a list of subscriptions to get the details from session storage.
-
-export default () => (
-    <form>
-        <h1>Subscribe</h1>
-        <hr style={{height:'5px', opacity:'1', backgroundColor:'black'}}></hr>
-        <label>Name:</label><input name="name" type="text" />
-        <br /><br /><label>Email:</label><input name="email" type="email" />
-        <br /><br /><input type='button' onClick={getInput()} value='Subscribe'/>
-    </form>    
-);
+export default Subscribe;
