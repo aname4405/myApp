31165a9709a735c92dc0333d3528798c23535932
aname4405 <156903740+aname4405@users.noreply.github.com>
2024-02-27 12:35:26 +0000

    Update about.jsx

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -4,6 +4,10 @@ export default () => (
     <div className='header'>
         <h1>About us</h1>
         <p>Welcome to The Band, we play lots of cool music.</p>
+        <br></br>
+        <p>Sign up to our newletter - click on Subscribe!</p>
+        <br></br>
+        <p>See us live - click on events!</p>
     </div>
     
 );
