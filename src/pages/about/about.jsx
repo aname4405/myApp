70114714ccd92c8af24a7256231526f5da2e3fa8
aname4405 <156903740+aname4405@users.noreply.github.com>
2024-02-27 12:36:49 +0000

    Update about.jsx

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -7,7 +7,9 @@ export default () => (
         <br></br>
         <p>Sign up to our newletter - click on Subscribe!</p>
         <br></br>
-        <p>See us live - click on events!</p>
+        <p>See us live - click on Events!</p>
+        <br></br>
+        <p>Buy some cool stuff - click on Merch!</p>
     </div>
     
 );
